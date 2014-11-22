bbfc05f334af64397b88a60c6964049ab0e0fff6
Rudy Mutter <rudy@yeti.co>
2014-11-22 15:02:52 -0800

    Update utils.py

--- collect/utils.py
+++ collect/utils.py
@@ -1,6 +1,6 @@
 from rest_framework.authtoken.models import Token
 from collect.models import User
 
-
+# This may just be leftover code, but you shouldn't be running something globally like this in a utils file
 for user in User.objects.all():
     Token.objects.get_or_create(user=user)
